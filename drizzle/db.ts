72239839521c1894509e262ff658369e75f40072
Jatin Kumar <jatinkumar10082003@gmail.com>
2024-11-23 12:58:05 +0530

    removed unnecessary files and folder and deps

--- drizzle/db.ts
+++
@@ -1,8 +0,0 @@
-import "./envConfig";
-import { drizzle } from "drizzle-orm/node-postgres";
-import { User, usersTable } from "./schema";
-export const db = drizzle(process.env.DATABASE_URL!);
-
-export const insertUser = async (user: User) => {
-  return db.insert(usersTable).values(user).execute();
-};
