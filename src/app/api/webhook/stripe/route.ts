15f8b34185ef6561c8ae8f99b566f0355cdf86d2
Jatin Kumar <jatinkumar10082003@gmail.com>
2025-04-28 22:41:27 +0530

    sending raw body

--- src/app/api/webhook/stripe/route.ts
+++ src/app/api/webhook/stripe/route.ts
@@ -4,12 +4,14 @@ import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
-  const body = await request.json();
-  const signature = (await headers()).get("stripe-signature") as string;
+  const rawBody = await request.text();
+
+  // 2. Get Stripe signature header
+  const signature = (await headers()).get("stripe-signature")!;
 
   try {
     const event = stripe.webhooks.constructEvent(
-      JSON.stringify(body),
+      rawBody,
       signature,
       process.env.STRIPE_WEBHOOK_SECRET!
     );
