d65ee2ad60579b1d8198df0117f70b3ffa178ad1
Jatin Kumar <jatinkumar10082003@gmail.com>
2025-04-28 22:55:18 +0530

    changes

--- src/app/api/webhook/stripe/route.ts
+++ src/app/api/webhook/stripe/route.ts
@@ -5,26 +5,24 @@ import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
   const rawBody = await request.text();
-
-  // 2. Get Stripe signature header
   const signature = (await headers()).get("stripe-signature")!;
-
+  let event;
   try {
-    const event = stripe.webhooks.constructEvent(
+    event = stripe.webhooks.constructEvent(
       rawBody,
       signature,
       process.env.STRIPE_WEBHOOK_SECRET!
     );
-    switch (event.type) {
-      case "checkout.session.completed":
-        HandleCheckoutSessionCompleted(event.data.object);
-        break;
-      default:
-        break;
-    }
-    return new NextResponse(null, { status: 200 });
   } catch (error) {
     console.error("stripe webhook error", error);
     return new NextResponse("webhook error", { status: 400 });
   }
+  switch (event.type) {
+    case "checkout.session.completed":
+      HandleCheckoutSessionCompleted(event.data.object);
+      break;
+    default:
+      break;
+  }
+  return new NextResponse(null, { status: 200 });
 }
